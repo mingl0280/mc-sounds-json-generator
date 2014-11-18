3ad9a07075860ddce5d7714b1f39bfe158c12457
mingl0280 <mingl0280@gmail.com>
2014-11-18 20:44:47 +0800

    init
    Signed-off-by: mingl0280 <mingl0280@gmail.com>

---
+++ MCSoundsJSONGenerator/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "声音文件夹"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(83, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(352, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(441, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 21)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "浏览"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(14, 25)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(230, 220)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "文件列表"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(250, 44)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 12)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "在游戏中的名称"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(250, 59)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(266, 21)
+        Me.TextBox2.TabIndex = 6
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(252, 25)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(144, 16)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "在游戏中生成为同一组"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(442, 98)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 21)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "浏览"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(250, 83)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 12)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "JSON文件"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(250, 98)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(185, 21)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(360, 222)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "生成"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(441, 222)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "退出"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.TextBox3)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Controls.Add(Me.TextBox2)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.ListBox1)
+        Me.Panel1.Enabled = False
+        Me.Panel1.Location = New System.Drawing.Point(-1, 33)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(528, 250)
+        Me.Panel1.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(528, 295)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "MusicJsonGen"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+
+End Class
---
+++ MCSoundsJSONGenerator/Form1.vb
@@ -0,0 +1,247 @@
+﻿Imports System.IO
+Imports System.Text
+
+Public Class Form1
+
+    Private Structure OGGFileInfo
+        Public Property filename
+            Set(value)
+                m_filename = value
+            End Set
+            Get
+                Return m_filename
+            End Get
+        End Property
+
+        Public Property catName
+            Set(value)
+                m_CatName = value
+            End Set
+            Get
+                Return m_CatName
+            End Get
+        End Property
+
+        Public Property FilePath
+            Set(value)
+                m_path = value
+            End Set
+            Get
+                Return m_path
+            End Get
+        End Property
+
+        Private m_CatName As String
+        Private m_filename As String
+        Private m_path As String
+
+        Public Sub New(ByVal filename As String, Optional ByVal catName As String = "", Optional ByVal path As String = "")
+            If path = "" Then
+                m_path = filename
+                m_filename = filename.Remove(".ogg")
+            Else
+                m_filename = filename
+                m_path = path
+            End If
+            If catName = "" Then
+                If m_filename.Length > 5 Then
+                    m_CatName = m_filename.Substring(0, 5).Replace(" ", "_")
+                Else
+                    m_CatName = m_filename.Replace(" ", "_")
+                End If
+            Else
+                m_CatName = catName
+            End If
+        End Sub
+    End Structure
+
+    Private OGGFileList As List(Of OGGFileInfo) = New List(Of OGGFileInfo)
+    Private oggFileArray As OGGFileInfo()
+    Private AllGroupName As String = ""
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FileDlg As New FolderBrowserDialog
+        Dim FRetVal = FileDlg.ShowDialog
+        If FRetVal = Windows.Forms.DialogResult.Cancel Then Exit Sub
+        If Directory.Exists(FileDlg.SelectedPath) = False Then Exit Sub
+        TextBox1.Text = FileDlg.SelectedPath
+        Panel1.Enabled = True
+        ListOggFiles()
+    End Sub
+
+    Private Sub TextBox1_Keydown(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyDown
+        If e.KeyData = Keys.Enter Then
+            If Directory.Exists(TextBox1.Text) = False Then Exit Sub
+            ListOggFiles()
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If Directory.Exists(TextBox1.Text) = False Then Panel1.Enabled = False And Button4.Enabled = True
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ListOggFiles()
+        Dim path As String = TextBox1.Text
+        Dim DirList As String() = Directory.GetFiles(path, "*.ogg")
+        For Each element As String In DirList
+            Dim filename As String = (New FileInfo(element)).Name
+            ListBox1.Items.Add(filename)
+            OGGFileList.Add(New OGGFileInfo(filename.Replace(".ogg", ""), , element))
+        Next
+        oggFileArray = OGGFileList.ToArray()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexAndValueChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged, ListBox1.SelectedValueChanged
+        If CheckBox1.Checked Then
+            TextBox2.Text = AllGroupName
+            Exit Sub
+        End If
+        TextBox2.Text = oggFileArray(ListBox1.SelectedIndex).catName
+        TextBox2.Focus()
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            Label3.Text = "所有组名"
+            TextBox2.Text = AllGroupName
+        Else
+            Label3.Text = "当前组名"
+            If ListBox1.SelectedIndex >= 0 Then TextBox2.Text = oggFileArray(ListBox1.SelectedIndex).catName
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If CheckBox1.Checked Then
+            AllGroupName = TextBox2.Text
+            Exit Sub
+        End If
+        oggFileArray(ListBox1.SelectedIndex).catName = TextBox2.Text
+    End Sub
+
+    Private Sub Panel1_EnabledChanged(sender As Object, e As EventArgs) Handles Panel1.EnabledChanged
+        If Panel1.Enabled And TextBox3.Text <> "" Then
+            Button3.Enabled = True
+        ElseIf Panel1.Enabled = True And TextBox3.Text = "" Then
+            Button3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim sdlg As SaveFileDialog = New SaveFileDialog
+        Dim result As DialogResult
+        With sdlg
+            .Filter = "json文件|*.json|文本文件|*.txt|所有文件|*.*"
+            result = .ShowDialog()
+            If result = Windows.Forms.DialogResult.Cancel Then Exit Sub
+            TextBox3.Text = .FileName
+        End With
+        Button3.Enabled = True
+    End Sub
+
+    Private Class JSONFile
+        Public catname As String
+        Public catcontent As String
+
+        ''' <summary>
+        ''' init a half-json file
+        ''' </summary>
+        ''' <param name="a">Catagory Name</param>
+        ''' <param name="b">Catagory Content</param>
+        ''' <remarks></remarks>
+        Public Sub New(Optional ByVal a As String = "", Optional ByVal b As String = "")
+            catname = a
+            catcontent = b
+        End Sub
+    End Class
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+
+            If CheckBox1.Checked = True Then
+                Dim jfile As JSONFile = New JSONFile(TextBox2.Text)
+                For i As Integer = 0 To ListBox1.Items.Count - 1
+                    jfile.catcontent += "       """ + oggFileArray(i).filename + """," + vbCrLf
+                Next
+                Dim sw As New StreamWriter(TextBox3.Text, False)
+                sw.Write("{" + vbCrLf + "  """ + jfile.catname + """: {" + vbCrLf + "    sounds: [" + vbCrLf + jfile.catcontent + "    ]" + vbCrLf + "  }" + vbCrLf + "}")
+                sw.Flush()
+            Else
+                Dim jfile As JSONFile()
+
+                For i As Integer = 0 To ListBox1.Items.Count - 1
+                    Dim tempJFile As New JSONFile(oggFileArray(i).catName, oggFileArray(i).filename)
+                    If i = 0 Then
+                        ReDim jfile(1)
+                        jfile(0) = New JSONFile
+                        jfile(1) = New JSONFile
+                    End If
+                    Dim existitem As Integer = -1
+                    For j As Integer = 0 To UBound(jfile)
+                        If jfile(j).Equals(Nothing) Then jfile(j) = New JSONFile
+                        If jfile(j).catname = tempJFile.catname Then
+                            existitem = j
+                            Exit For
+                        End If
+                    Next
+                    If existitem >= 0 Then
+                        jfile(existitem).catcontent += "," + vbCrLf + "       """ + tempJFile.catcontent + """"
+                    Else
+                        Dim emptyIndex As Integer = FindFirstEmptyIndex(jfile)
+                        If emptyIndex = -1 Then
+                            ReDim Preserve jfile(jfile.Length)
+                            jfile(UBound(jfile)) = New JSONFile
+                            jfile(UBound(jfile)).catname = tempJFile.catname
+                            jfile(UBound(jfile)).catcontent = "       """ + tempJFile.catcontent + """"
+                        Else
+                            jfile(emptyIndex).catname = tempJFile.catname
+                            jfile(emptyIndex).catcontent = "       """ + tempJFile.catcontent + """"
+                        End If
+                    End If
+                Next
+
+                Dim beginStr As String = "{"
+
+                For t As Integer = 0 To UBound(jfile)
+                    beginStr = beginStr + vbCrLf + "  """ + jfile(t).catname + """: {" + vbCrLf + "    sounds: [" + vbCrLf + jfile(t).catcontent + vbCrLf + "    ]" + vbCrLf + "  }"
+                Next
+
+                beginStr += vbCrLf + "}"
+                Dim sw As New StreamWriter(TextBox3.Text)
+                sw.Write(beginStr)
+                sw.Flush()
+            End If
+            MessageBox.Show("写入文件" + TextBox3.Text + "完成", "信息", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "oops,不小心出错了！", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End Try
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        If TextBox3.Text <> "" Then Button3.Enabled = True
+    End Sub
+
+    Private Function FindFirstEmptyIndex(ByVal k As JSONFile()) As Integer
+        For i As Integer = 0 To k.Length - 1
+            If k(i).catname = "" And k(i).catcontent = "" Then Return i
+        Next
+        Return -1
+    End Function
+
+    Private Function FindItemIndex(ByVal content As String) As Integer
+        For i As Integer = 0 To oggFileArray.Length - 1
+            If content = oggFileArray(i).filename Then Return i
+        Next
+        Return -1
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button4.Parent = Me
+        Button4.BringToFront()
+        Button4.Location = New Point(Button4.Location.X, Button3.Location.Y + Panel1.Location.Y)
+    End Sub
+End Class
+
